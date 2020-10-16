8ed187937737dd89b576b64f5e0ba94ff0db5921
Alex Perea <ing.alexperea@gmail.com>
2020-10-16 17:21:16 -0500

    log configured

--- script.php
+++ script.php
@@ -8,19 +8,30 @@ $client = new Goutte\Client();
 
 $url = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440&intl=nosplash#headerskip';
 
-$crawler = $client->request('GET', $url);
-$data = $crawler->html();
+try {
+    $crawler = $client->request('GET', $url);
+    $data = $crawler->html();
 
-$matchesSoldOut = preg_match_all('/sold\sout/im', $data);
-$matchesComingSoon = preg_match_all('/coming\ssoon/im', $data);
+    $matchesSoldOut = preg_match_all('/sold\sout/im', $data);
+    $matchesComingSoon = preg_match_all('/coming\ssoon/im', $data);
 
-var_dump($matchesSoldOut);
+    $msg = date('Y-m-d H:i:s') . ' : Matches sould out: ' . $matchesSoldOut . ' Matches coming soon: ' . $matchesComingSoon;
 
-if ($matchesSoldOut == 0 || $matchesComingSoon > 0) {
-    sendNotifications();
+    writeToLog($msg);
+
+    // if ($matchesSoldOut == 0 || $matchesComingSoon > 0) {
+        sendNotifications();
+    // }
+} Catch (Exception $e){
+    $msg = date('Y-m-d H:i:s') . ' : Exception: ' . $e->getMessage();
+    writeToLog($msg);
 }
 
+
 function sendNotifications() {
+
+    $msg = date('Y-m-d H:i:s') . ' : Sending twilio notification';
+    writeToLog($msg);
 
     // Your Account SID and Auth Token from twilio.com/console
     $account_sid = 'ACad7db494ca14dec5e6b8800ee0faab51';
@@ -42,4 +53,20 @@ function sendNotifications() {
             "url" => "http://demo.twilio.com/docs/voice.xml"
         )
     );
+
+    $msg = date('Y-m-d H:i:s') . ' : Twilio notification sent';
+    writeToLog($msg);
+}
+
+function writeToLog($msg) {
+    
+    $file_path = dirname(__FILE__)  . '/log.txt';
+    $file_handle = fopen($file_path, 'r'); 
+    $contents = fread($file_handle, filesize($file_path));
+    fclose($file_handle);
+
+    $contents .= "\n" . $msg;
+    $file_handle = fopen($file_path, 'w');
+    fwrite($file_handle, $contents);
+    fclose($file_handle);
 }
